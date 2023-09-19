fa34e187431b105de9ae1a815b76b5ff83d6d9b9
abdulhakim-altunkaya <altunkaya2@gmail.com>
2023-09-20 00:24:20 +0300

    input component created together with firebase config file. Connection is build. Major update

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import AddSentences from "./components/AddSentences";
+
+function App() {
+  return (
+    <div className="App">
+      <AddSentences/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AddSentences.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import {FIREBASE_DB} from "../firebaseConfig";
+import { addDoc, collection, deleteDoc, updateDoc, onSnapshot, doc  } from 'firebase/firestore';
+
+function AddSentences() {
+
+  const [formData, setFormData] = useState({
+    engSentence: "",
+    turSentence: ""
+  });
+
+  const handleChange = (e) => {
+    let inputName = e.target.name;
+    let inputValue = e.target.value;
+    setFormData({
+      ...formData, 
+      [inputName]: inputValue
+    })
+  }
+  
+
+  const saveSentences = async (e) => {
+    e.preventDefault();
+    await addDatabase();
+  }
+  const addDatabase = async () => {
+    await addDoc(collection(FIREBASE_DB, "sentences"), {
+      english: formData.engSentence,
+      turkish: formData.turSentence
+    })
+  }
+
+  return (
+    <div className="input-container">
+      <form onSubmit={saveSentences}>
+        <input className="custom-input" type="text" placeholder="English Sentence"
+        value={formData.engSentence} onChange={handleChange}/>
+        <input className="custom-input" type="text" placeholder="Turkish Sentence" 
+        value={formData.turSentence} onChange={handleChange}/>
+        <button className="custom-button">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default AddSentences;
